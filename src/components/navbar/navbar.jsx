cc829eae8a14b248d19ede2c2a632330c9d5139b
Iosif Screpciuc <screpy@iosifs-air.home>
2022-11-09 14:15:24 +0000

    only css but works

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,50 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LinkStyle, NavWrapper, NavigationMenu, BrandName, UlStyle, BurgerWrapper, LiStyle } from './navbar.style';
+import './navstyles.css'
 
-const Navbar = ({isNavExpanded, setIsNavExpanded}) => {
+const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   
-  const handleExpanded = () => {
-    setIsNavExpanded(!isNavExpanded);
-    console.log(isNavExpanded)
-  }
-
-  const closeSideBar = () => {
-    setIsNavExpanded(false)
-  }
   return (
-    <NavWrapper>
-      <BrandName href="/" className="brand-name">
-        Detailing Auto D&S
-      </BrandName>
-      <BurgerWrapper onClick={() => handleExpanded()}>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
-       <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>s
-      </BurgerWrapper>
-      {isNavExpanded ? (
-      <NavigationMenu onClick={closeSideBar}>
-          <UlStyle setIsNavExpanded={setIsNavExpanded} >
-            <LiStyle>
-              <LinkStyle to='/'>Home</LinkStyle>
-            </LiStyle>
-            <LiStyle>
-              <LinkStyle to='/despre-noi'>Despre</LinkStyle>
-            </LiStyle>
-            <LiStyle>
-              <LinkStyle to='/galerie'>Galerie</LinkStyle>
-            </LiStyle>
-            <LiStyle>
-              <LinkStyle to='/pachete'>Pachete</LinkStyle>
-            </LiStyle>
-            <LiStyle>
-              <LinkStyle to='/contact'>Contact</LinkStyle>
-            </LiStyle>
-          </UlStyle>
-        
-      </NavigationMenu>
-      ):(<></>)}
-    </NavWrapper>
-  );
-};
+    <>
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        MacroSoft
+      </a>
+      <button className="hamburger" onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+      className={
+        isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+      }
+      >
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </>
+)};
 
 export default Navbar;
