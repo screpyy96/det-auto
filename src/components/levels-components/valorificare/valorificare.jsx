2edc56df347538b449cdb3c0f96d5dcdd9b22d86
Iosif Screpciuc <screpy@iosifs-air.home>
2022-11-18 13:21:55 +0000

    Asd

--- src/components/levels-components/valorificare/valorificare.jsx
+++ src/components/levels-components/valorificare/valorificare.jsx
@@ -3,19 +3,74 @@ import React, { useState } from 'react'
 import { ValorificaComponent } from './valorifica-component'
 
 const Valorificare = () => {
-    const [isShown, setIsShown] = useState(false)
+    const [isShown, setIsShown] = useState(true)
+    const [isShown1, setIsShown1] = useState(false)
+    const [isShown2, setIsShown2] = useState(false)
+    const [isShown3, setIsShown3] = useState(false)
+    const [isShown4, setIsShown4] = useState(false)
 
-    const handleClickEvent = () => {
-        setIsShown(current => !current)
+    const handleClickEvent1 = () => {
+        setIsShown(!isShown)
     }
+    const handleClickEvent2 = () => {
+      setIsShown(!isShown)
+      setIsShown1(!isShown1)
+    }
+    
+    
+    const handleClickEvent3 = () => {
+      setIsShown1(!isShown1)
+        setIsShown2(!isShown2)
+    }
+    const handleClickEvent4 = () => {
+      setIsShown2(!isShown2)
+        setIsShown3(!isShown3)
+
+    }
+    const handleClickEvent5 = () => {
+      setIsShown3(isShown3)
+        setIsShown4(!isShown4)
+    }
+
   return (
-    <div>
+    <div style={{display: "inline-flex"}}>
         <div>
-            <Button variant='contained' color='primary' onClick={handleClickEvent}>Level 1</Button>
+            <Button variant='contained' color='primary' onClick={handleClickEvent1}>Level 1</Button>
             {isShown && (
-            <ValorificaComponent />
-                  )}
+            // <ValorificaComponent />
+            <h2>level1</h2>
+            )}
         </div>
+        <div>
+            <Button variant='contained' color='primary' onClick={handleClickEvent2}>Level 2</Button>
+            {isShown1 && (
+            // <ValorificaComponent />
+            <h2>level2</h2>
+            )}
+        </div>
+        <div>
+            <Button variant='contained' color='primary' onClick={handleClickEvent3}>Level 3</Button>
+            {isShown2 && (
+            // <ValorificaComponent />
+            <h2>level3</h2>
+            )}
+        </div>
+        <div>
+            <Button variant='contained' color='primary' onClick={handleClickEvent4}>Level 4</Button>
+            {isShown3 && (
+            // <ValorificaComponent />
+            <h2>level4</h2>
+            )}
+        </div>
+        <div>
+            <Button variant='contained' color='primary' onClick={handleClickEvent5}>Level 5</Button>
+            {isShown4 && (
+            // <ValorificaComponent />
+            <h2>level5</h2>
+            )}
+        </div>
+
+       
     </div>
   );
 }
